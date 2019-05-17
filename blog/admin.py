4cc68669c9185aaeec541f40bb8d40b8f2a69a45
Faanagor <faanagor@gmail.com>
2019-05-17 11:54:50 -0500

    Cambie el HTML para la pgina.

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,24 @@
 from django.contrib import admin
 from .models import Post
+from django.http import HttpResponse
+
+from django.contrib.admin import AdminSite
 
 admin.site.register(Post)
+
+
+
+class MyAdminSite(AdminSite):
+
+     def get_urls(self):
+         from django.conf.urls import url
+         urls = super(MyAdminSite, self).get_urls()
+         urls += [
+             url(r'^my_view/$', self.admin_view(self.my_view))
+         ]
+         return urls
+
+     def my_view(self, request):
+         return HttpResponse("Hello!")
+
+admin_site = MyAdminSite()
